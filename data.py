c98282af69b7fb64d0ce048ce5dc158a4c79f6f2
strnadaljaz <strnadaljaz23@gmail.com>
2026-04-14 18:19:28 +0200

    upload commit

---
+++ data.py
@@ -0,0 +1,34 @@
+from skyfield.api import load
+from zoneinfo import ZoneInfo
+from datetime import timedelta
+
+# Gets when ISS could be visible from location 
+# you specify in time window from now to 24 hours later.
+# Doesn't take into account the weather conditions and 
+# sun position
+def getData(dataUrl: str, timeSystem, location):
+    
+    # loads satellites data from url and 
+    # searches for ISS
+    satellites = load.tle_file(dataUrl)
+    by_name = {sat.name: sat for sat in satellites}
+    iss = by_name['ISS (ZARYA)']
+
+    # Current time and time 24 hours later
+    t0 = timeSystem.now()
+    t1 = timeSystem.from_datetime(t0.utc_datetime() + timedelta(days=1))
+
+    # Gets events and times
+    # Events: - 0 for beginning
+    #         - 1 for ideal position
+    #         - 2 for the end of visible window
+    times, events = iss.find_events(location, t0, t1, 10.0)
+
+    # Convert times from UTC to my local times
+    localTimes = []
+
+    for t in times:
+        localTime = t.utc_datetime().astimezone(ZoneInfo("Europe/Ljubljana"))
+        localTimes.append(localTime)
+
+    return localTimes, events
---
+++ tracker.py
@@ -0,0 +1,39 @@
+from skyfield.api import load, wgs84
+import time
+from data import getData
+from colorama import Fore, Style, init
+import os
+from dotenv import load_dotenv, dotenv_values
+
+init()
+load_dotenv()
+
+LAT = float(os.getenv("LATITUDE"))
+LON = float(os.getenv("LONGITUDE"))
+ELEV = float(os.getenv("ELEVATION"))
+
+myPosition = wgs84.latlon(LAT, LON, ELEV)
+
+url = 'http://celestrak.org/NORAD/elements/stations.txt'
+
+timeSystem = load.timescale()
+
+localTimes, events = getData(url, timeSystem, myPosition)
+
+for i in range(len(localTimes)):
+    
+    if events[i] == 0:
+        print(Fore.YELLOW + "Start:" + str(localTimes[i]))
+    elif events[i] == 1:
+        print(Fore.GREEN + "Ideal:" + str(localTimes[i]))
+    else:
+        print(Fore.RED + "End:" + str(localTimes[i]))
+
+'''
+while (True):
+    if gotData:
+        time.sleep(600)
+
+    else:
+        localTimes = getData(url, timeSystem, myPosition)
+'''
