e8ed3110a0e4334ed141114c6dd962574d203bd5
strnadaljaz <strnadaljaz23@gmail.com>
2026-04-21 17:39:09 +0200

    added a track.py, that waits for a http request and sends back altitude and azimuth. Also added newly needed package to the requirements.txt (fastapi)

---
+++ tracker.py
@@ -0,0 +1,40 @@
+from data import getIssAltitudeAndAzimuth
+from skyfield.api import load, wgs84
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import os
+
+# Need this for the requests
+app = FastAPI()
+
+load_dotenv()
+
+LAT = float(os.getenv("LATITUDE"))
+LON = float(os.getenv("LONGITUDE"))
+ELEV = float(os.getenv("ELEVATION"))
+
+myPosition = wgs84.latlon(LAT, LON, ELEV)
+
+timeSystem = load.timescale()
+
+# Wait for the request from module esp32
+@app.get("/iss")
+def sendDataToModule():
+    # Current time
+    timeNow = timeSystem.now()
+
+    # Altitude and azimuth
+    alt, az = getIssAltitudeAndAzimuth(timeNow, myPosition)
+    
+    # If ISS is under horizon, we don't send data
+    if alt < 0: 
+        return {
+            "visible": False
+        }
+
+    # If ISS is visible, we send altitude and azimuth rounded to one decimal
+    return {
+        "az": round(az, 1),
+        "el": round(alt, 1),
+        "visible": True
+    }
